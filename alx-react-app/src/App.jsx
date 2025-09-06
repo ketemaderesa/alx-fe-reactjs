3a173e4a135c9353670d66d5a2ac092e3b8b3b1c
Ketema Deresa <ketemaderesa123@gmail.com>
2025-09-06 21:25:20 +0100

    Create UserProfile component using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 function App() {
   return (
     <div>
@@ -9,6 +10,8 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile name="Bob" age="30" bio="Enjoys painting and travel" />
     </div>
   );
 }
