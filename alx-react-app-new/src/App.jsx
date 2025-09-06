030aeb1db66920aa7c01c5c56a5e3d50459c0614
Ketema Deresa <ketemaderesa123@gmail.com>
2025-09-06 21:42:11 +0100

    Apply inline CSS styling to Header, UserProfile, MainContent, Footer

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,21 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+function App() {
+  return (
+    <>
+    <div>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile name="Bob" age="30" bio="Enjoys painting and travel" />
+    </div>
+  </>
+  );
+}
+
+export default App;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return (
+<main style={{ padding: '15px', fontSize: '16px', color: 'darkgreen' }}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    );
+}
+export default MainContent;
