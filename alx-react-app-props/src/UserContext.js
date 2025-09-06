9f70d7141c168ee381179d362f12958cbd821066
Ketema Deresa <ketemaderesa123@gmail.com>
2025-09-06 22:10:47 +0100

    Refactor prop drilling to Context API

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,6 +1,8 @@
 // src/UserContext.js
-import { createContext } from 'react';
+import { createContext } from "react";
 
+// Create a context
 const UserContext = createContext();
 
+// Export it as default
 export default UserContext;
