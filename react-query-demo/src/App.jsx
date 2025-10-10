166ddf7b66db8ed4089aac862717df58f1a5c276
Ketema Deresa <ketemaderesa123@gmail.com>
2025-10-10 15:24:42 +0100

    fix: stage and commit changes in react-query-demo

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,14 +1,29 @@
-import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import PostsComponent from "./components/PostsComponent.jsx"; // <-- now this exists
-
-const queryClient = new QueryClient();
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import BlogPost from './pages/BlogPost';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <PostsComponent />
-    </QueryClientProvider>
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/blog/1">Blog 1</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={true}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </div>
   );
 }
 
