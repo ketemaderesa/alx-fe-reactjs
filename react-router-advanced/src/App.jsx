752fdea31d477d4a8c50673035473cd42b96b779
Ketema Deresa <ketemaderesa123@gmail.com>
2025-10-11 06:45:08 +0100

    feat: implement advanced routing (nested, dynamic, protected) using React Router v6

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,6 +1,5 @@
-// src/App.jsx
 import React from 'react'
-import { Routes, Route, Link, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Profile from './pages/Profile'
@@ -13,45 +12,41 @@ import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>React Router Advanced Demo</h1>
+    <BrowserRouter>
+      <div style={{ padding: 20 }}>
+        <h1>React Router Advanced Demo</h1>
 
-      <nav style={{ marginBottom: 20 }}>
-        <Link to="/">Home</Link> |{' '}
-        <Link to="/about">About</Link> |{' '}
-        <Link to="/profile">Profile</Link> |{' '}
-        <Link to="/blog">Blog</Link> |{' '}
-        <Link to="/login">Login</Link>
-      </nav>
+        <nav style={{ marginBottom: 20 }}>
+          <Link to="/">Home</Link> |{' '}
+          <Link to="/about">About</Link> |{' '}
+          <Link to="/profile">Profile</Link> |{' '}
+          <Link to="/blog">Blog</Link> |{' '}
+          <Link to="/login">Login</Link>
+        </nav>
 
-      <Routes>
-        {/* Basic routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-        {/* Protected + Nested routes */}
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
 
-        {/* Dynamic route for blog posts */}
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:postId" element={<BlogPost />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:postId" element={<BlogPost />} />
 
-        {/* Login route */}
-        <Route path="/login" element={<Login />} />
-
-        {/* Catch-all route */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </div>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
